ca560cb5b34036b0e2ad1d09171cae909d05884d
Fatih <fatih.bulbul@datasoft.web.tr>
2020-02-07 12:06:39 +0300

    Add project files.

---
+++ Controllers/EvaluateController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using System.Threading.Tasks;
+
+namespace DatetimeArge.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EvaluateController : ControllerBase {
+        // POST api/values
+        [HttpPost]
+        public Task<object> Post([FromBody] string value) {
+            return CSharpScript.EvaluateAsync(value);
+        }
+
+    }
+}
